e31cbf807e176149e84f84ca3fd455189b92ad87
Varsha7330 <168283899+Varsha7330@users.noreply.github.com>
2026-05-08 12:24:45 +0530

    Create queries.sql

---
+++ week1/day3/queries.sql
@@ -0,0 +1,56 @@
+Below is the code for creating a table and insert the data into the table
+CREATE TABLE Employees (
+    emp_id INT,
+    emp_name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT,
+    city VARCHAR(50),
+    experience INT
+);
+
+INSERT INTO Employees VALUES
+(101, 'Rahul', 'IT', 75000, 'Hyderabad', 5),
+(102, 'Anjali', 'HR', 45000, 'Chennai', 3),
+(103, 'Kiran', 'IT', 82000, 'Bangalore', 6),
+(104, 'Sneha', 'Finance', 67000, 'Hyderabad', 4),
+(105, 'Aman', 'HR', 39000, 'Pune', 2),
+(106, 'Ravi', 'Finance', 91000, 'Mumbai', 8),
+(107, 'Divya', 'IT', 55000, 'Chennai', 3),
+(108, 'Meena', 'Sales', 48000, 'Bangalore', 2),
+(109, 'Arjun', 'Sales', 61000, 'Hyderabad', 5),
+(110, 'Pooja', 'IT', 73000, 'Mumbai', 4),
+(111, 'Vikas', 'HR', 52000, 'Pune', 3),
+(112, 'Nisha', 'Finance', 88000, 'Bangalore', 7),
+(113, 'Tarun', 'Sales', 46000, 'Chennai', 2),
+(114, 'Kavya', 'IT', 97000, 'Hyderabad', 9),
+(115, 'Manoj', 'Finance', 58000, 'Mumbai', 4);
+
+//Queries
+--SELECT
+--1.Display all employee details.
+select *from Employees;
+--2.Display only employee names and salaries.
+select emp_name,salary from Employees;
+--3.Display employee names and departments.
+select emp_name,department from Employees;
+--4.Display all employees from the IT department.
+select emp_name from Employees where department='IT';
+--5.Display employee names and experience.
+select emp_name, experience from Employees;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
