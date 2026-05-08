9bc5d8ad793e6247af5524fdb17a7131e1441882
Varsha7330 <168283899+Varsha7330@users.noreply.github.com>
2026-05-08 14:51:55 +0530

    Update queries.sql

--- week1/day3/queries.sql
+++ week1/day3/queries.sql
@@ -38,6 +38,94 @@ select emp_name from Employees where department='IT';
 --5.Display employee names and experience.
 select emp_name, experience from Employees;
 
+--WHERE
+--Find employees with salary greater than 70000.
+select emp_name from Employees where salary>70000;
+--Find employees working in Hyderabad.
+select emp_name from Employees where lower(city)="hyderabad";
+--Find employees with experience less than 4 years.
+select emp_name from Employees where experience<4;
+--Find employees from Finance department.
+select emp_name from Employees where lower(department)='finance';
+--Find employees whose salary is equal to 52000.
+select emp_name from Employees where salary= 52000;
+
+--GROUP BY
+--Find total salary department-wise.
+select department, sum(salary) as total_salary from Employees
+group by department;
+--Find average salary in each department.
+select department, avg(salary) as avg_salary from Employees
+group by department;
+--Count employees in each city.
+select city, count(*) from Employees
+group by city;
+--Find maximum salary in each department.
+select department, max(salary) as max_salary from Employees
+group by department;
+--Find minimum experience department-wise.
+select department, min(experience) as min_experience from Employees
+group by department;
+
+--HAVING
+--Find departments having more than 3 employees.
+select department, count(*) as total_employees from Employees
+group by department
+having count(*)>3;
+--Find departments where average salary is greater than 60000.
+select department, avg(salary) as avg_salary from Employees
+group by department
+having avg(salary)>60000
+--Find cities having more than 2 employees.
+SELECT city, COUNT(*) AS total_employees
+FROM Employees
+GROUP BY city
+HAVING COUNT(*) > 2;
+--Find departments where total salary is greater than 200000.
+SELECT department, SUM(salary) AS total_salary
+FROM Employees
+GROUP BY department
+HAVING SUM(salary) > 200000;
+--Find departments where maximum salary is above 90000.
+SELECT department, MAX(salary) AS max_salary
+FROM Employees
+GROUP BY department
+HAVING MAX(salary) > 90000;
+
+--TOP
+--Display top 5 highest paid employees.
+SELECT *
+FROM Employees
+ORDER BY salary DESC
+LIMIT 5;
+--Display top 3 employees with highest experience.
+select * from Employees
+order by experience DESC
+limit 3;
+--Display top 2 salaries from Finance department.
+select * from Employees where department="Finance"
+order by salary DESC
+limit 2;
+--Display top 4 employees from Hyderabad.
+select * from Employees where city= 'Hyderabad'
+limit 4;
+--Display top 1 highest salary employee.
+select * from Employees 
+order by salary DESC
+limit 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
