cb105ab0edcbe846924bc4563a03d685b27e2d21
Varsha7330 <168283899+Varsha7330@users.noreply.github.com>
2026-05-16 11:20:50 +0530

    Create orders_delivery table and delivery queries
    Added a new table for order deliveries with related queries.

--- week 2/StrDataNum/queries.sql
+++ week 2/StrDataNum/queries.sql
@@ -1,3 +1,5 @@
+
+--NUMBER 1
 CREATE TABLE employee_payments (
     emp_id INT PRIMARY KEY,
     emp_name VARCHAR(50),
@@ -51,4 +53,51 @@ SELECT
     END AS employee_category
 
 FROM employee_payments;
+
+
+--NUMBER 2
+CREATE TABLE orders_delivery (
+order_id INT,
+customer_name VARCHAR(50),
+order_date DATE,
+delivery_date DATE,
+order_amount DECIMAL(10,2)
+);
+INSERT INTO orders_delivery VALUES
+(101,'rajesh','2025-01-01','2025-01-05',12500.75),
+(102,'meena','2025-01-10','2025-01-10',8400.40),
+(103,'arun','2025-01-15','2025-01-20',15600.90),
+(104,'pooja','2025-01-18',NULL,9200.10);
+--For each order:
+--· Uppercase customer name
+--· Calculate delivery days using date difference
+--· Replace NULL delivery date with today
+--· Truncate order amount to 1 decimal
+--· Use CASE:
+--o Same-day
+--o Delayed (>3 days)
+--o Pending
+select 
+	order_id,
+    upper(customer_name) as customer_name,
+     DATEDIFF(
+        COALESCE(delivery_date, CURDATE()),
+        order_date
+    ) AS delivery_days,
+    COALESCE(delivery_date, CURDATE()) AS final_delivery_date,
+    TRUNCATE(order_amount,1) AS truncated_amount,
+    case
+    	when delivery_date is NULL
+        	then 'pending'
+        when DATEDIFF(delivery_date, order_date)=0
+        	then 'same-day'
+        when DATEDIFF(delivery_date, order_date)> 3
+        	then 'Delayed'
+        else 'normal'
+    end as delivery_status
+from orders_delivery;
+
+
+		
+
 
