c71cc99ac96a0ac4229a19ce48bdd8ecca5bd8db
Varsha7330 <168283899+Varsha7330@users.noreply.github.com>
2026-05-16 10:59:27 +0530

    Update queries.sql

--- week 2/StrDataNum/queries.sql
+++ week 2/StrDataNum/queries.sql
@@ -1,1 +1,54 @@
+CREATE TABLE employee_payments (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department VARCHAR(30),
+    base_salary DECIMAL(10,2),
+    bonus DECIMAL(10,2),
+    joining_date DATE
+);
+INSERT INTO employee_payments VALUES
+(1,'karthik','Data',75000.75,5000.50,'2019-03-15'),
+(2,'veena','HR',65000.40,4000.25,'2021-06-20'),
+(3,'ravi','Data',85000.90,6000.75,'2016-01-10'),
+(4,'anil','Finance',70000.10,NULL,'2020-09-01'),
+(5,'suresh','HR',60000.55,3000.30,'2022-11-25');
+
+--QUESTION
+--For each employee:  
+--Convert emp_name to proper case ---upper /lower ---Initcap (CamelCase)
+--Calculate total income = base_salary + bonus (NULL safe) +
+--Round total income to nearest integer
+--Extract joining year
+--Use CASE to classify:
+-- Senior if experience > 7 years
+--Mid if between 4 and 7
+--Junior otherwise
+
+SELECT 
+    emp_id,
+
+    UPPER(emp_name) AS upper_name,
+    LOWER(emp_name) AS lower_name,
+
+    CONCAT(
+        UPPER(LEFT(emp_name,1)),
+        LOWER(SUBSTRING(emp_name,2))
+    ) AS proper_name,
+
+    ROUND(base_salary + COALESCE(bonus,0)) AS total_income,
+
+    YEAR(joining_date) AS joining_year,
+
+    CASE
+        WHEN TIMESTAMPDIFF(YEAR, joining_date, CURDATE()) > 7 
+            THEN 'Senior'
+
+        WHEN TIMESTAMPDIFF(YEAR, joining_date, CURDATE()) 
+             BETWEEN 4 AND 7 
+            THEN 'Mid'
+
+        ELSE 'Junior'
+    END AS employee_category
+
+FROM employee_payments;
 
